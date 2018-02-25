7999011afba658ab570bc1693e860eb310f2eff9
gregornemec <gregorneme@gmail.com>
2018-02-25 20:34:27 +0000

    Ulicna rasvetljava testiranje.

--- Ulicna_rasvetljava/ulicna_rasvetljava.py
+++ Ulicna_rasvetljava/ulicna_rasvetljava.py
@@ -1,6 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-
-from gpiozero import LED, LEDBoard
+from gpiozero import LED, LEDBoard, PWMLED
 from time import sleep
+from signal import pause
+
+#Pin nubering is BCM and not BOARD numbering like in RPi.GPIO.
+
+#led = PWMLED(17)
+leds = LEDBoard(4, 17, 18, 27, 22, 23, 24, 25, 5, 6, pwm=True)  
 
 
+leds.value = (0.0, 0.2, 0.0, 0.2, 0.0, 0.2, 0.0, 0.2, 0.0, 0.2)
+
+# brightnes = 1.0
+# while True:
+#     led.value = brightnes
+#     sleep(0.1)
+    
+#     if brightnes >= 0.1:
+#         brightnes -= 0.1
+#         brightnes = round(brightnes, 1)
+#     elif brightnes == 0.0:
+#         brightnes = 1.0
+
+#     print("Brightne: ", brightnes)
+
+
+
+pause()
