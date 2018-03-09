3662ccb8d6816346b55736b7e391f94537b7f5c2
gregornemec <gregorneme@gmail.com>
2018-03-09 11:25:56 +0100

    Semafor pesci.

--- Projekti/Prometna_signalizacija/code/semafor_pesci.py
+++ Projekti/Prometna_signalizacija/code/semafor_pesci.py
@@ -4,34 +4,47 @@
 import gpiozero
 import time
 
+
+#inicializacija LED
+
 #inicializacija semafor pešci
-led = gpiozero.LED(27)
-
-#inicializacija piskača
-zvok = gpiozero.Buzzer(21)
-
-#inicializacija gumb
-gumb = gpiozero.Button(20)
-
-
-def signal(t):
-    led.on()
-    zvok.on()
-    print("LED on")
-    time.sleep(t)
-    led.off()
-    zvok.off()
-    print("LED off")
-    time.sleep(t)
-
+pesci_zelena = gpiozero.LED(5)
+pesci_rdeca = gpiozero.LED(6)
 
 while True:
-    if gumb.is_held:
-        signal(0.1)        
-        print("Držim")
-    else:
-        signal(1)
-        print("Ne držim")
+    pesci_rdeca.on()
+    pesci_zelena.off()
+    time.sleep(3)
+
+    pesci_rdeca.off()
+    pesci_zelena.on()
+    time.sleep(1)
+
+#inicializacija piskača
+# zvok = gpiozero.Buzzer(21)
+
+# #inicializacija gumb
+# gumb = gpiozero.Button(20)
+
+
+# def signal(t):
+#     led.on()
+#     zvok.on()
+#     print("LED on")
+#     time.sleep(t)
+#     led.off()
+#     zvok.off()
+#     print("LED off")
+#     time.sleep(t)
+
+
+# while True:
+#     if gumb.is_held:
+#         signal(0.1)        
+#         print("Držim")
+#     else:
+#         signal(1)
+#         print("Ne držim")
 
 
 
