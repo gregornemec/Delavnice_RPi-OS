30269ac7eb8c4436c23ad10fadf2d0b10b7995df
gregornemec <gregorneme@gmail.com>
2018-03-09 09:45:35 +0100

    beeb!

--- Projekti/Prometna_signalizacija/code/semafor_pesci.py
+++ Projekti/Prometna_signalizacija/code/semafor_pesci.py
@@ -10,9 +10,11 @@ zvok = gpiozero.Buzzer(21)
 
 while True:
     led.on()
+    zvok.on()
     print("LED on")
     time.sleep(1)
     led.off()
+    zvok.off()
     print("LED off")
     time.sleep(1)
 
