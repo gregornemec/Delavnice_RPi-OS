be9834efa89dd120cd6c1b9945c80931c684ce35
gregornemec <gregorneme@gmail.com>
2018-04-18 11:30:27 +0200

    Tested code for button.

---
+++ 05_RGB_LED/code/02_RGB_LED-Gumb.koncen.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env Pyhrn
+# -*- coding: utf-8 -*-
+
+import time
+import RPi.GPIO as GPIO
+
+red = 11
+blue = 13
+green = 13
+
+# Inicializacija pinov za posamezni priklop
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(red, GPIO.OUT)       # Določimo način pina na izhod.
+GPIO.output(red, GPIO.HIGH)     # Napetost na pinu red ugasnemo (HIGH).
+GPIO.setup(blue, GPIO.OUT)
+GPIO.output(blue, GPIO.HIGH)
+GPIO.setup(green, GPIO.OUT)
+GPIO.output(green, GPIO.HIGH)
+
+
+# Inicializacija gumba
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Inicializacijo dodaj se za ostala gumba
+
+
+try:
+    # Z zanko while True ponavljamo program neskončno krat.
+    while True:
+        # Preverjaj stanje gumbov
+        rdec_pritisnjen = GPIO.input(36)
+        zelen_pritisnjen = GPIO.input(38)
+        moder_pritisnjen = GPIO.input(40)
+
+        # Vklop rdece
+        if rdec_pritisnjen == False:
+            print('Rdeč je pritisnjen!')
+            GPIO.output(red, GPIO.LOW)
+            time.sleep(0.2)
+        else:
+            GPIO.output(red, GPIO.HIGH)
+
+        # Dopolni Vklop zelene
+
+        # Dopolni Vklop modre
+
+
+except KeyboardInterrupt:
+    # Za prekinitev programa uporabi kombinacijo C-c. Ko program zazna
+    # prekinitev sproži pokliče še spodnje.
+
+    GPIO.output(red, GPIO.HIGH)  # Ugasni led
+    GPIO.output(blue, GPIO.HIGH)
+    GPIO.output(green, GPIO.HIGH)
+    GPIO.cleanup()              # Počisti GPIO.
