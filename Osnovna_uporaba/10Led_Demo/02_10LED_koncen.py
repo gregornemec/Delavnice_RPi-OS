00d188a6817ba53bcd6c1af95706ec97399502e9
gregornemec <gregorneme@gmail.com>
2019-10-22 21:47:43 +0200

    Adding new demo project 10LED.

--- Osnovna_uporaba/10Led_Demo/02_10LED_koncen.py
+++ Osnovna_uporaba/10Led_Demo/02_10LED_koncen.py
@@ -6,39 +6,66 @@ import RPi.GPIO as GPIO
 
 # Shranjevanje posameznih priklopov LED na GPIO v seznamu
 
-pins = [7, 11, 12, 13, 15, 16, 18, 22,  29, 31]
+pins = [7, 11, 13, 15, 12, 16, 18, 22]
 
 def nastavi():
     '''Nastavljanje začetnih nastavitev.'''
 
     GPIO.setmode(GPIO.BOARD)
     for pin in pins:
-        GPIO.setup(pin, GPIO.out)
+        GPIO.setup(pin, GPIO.OUT)
 
     ugasni_vse()
+
 
 def pocisti():
     '''Pocisti za konec programa.'''
     ugasni_vse()
     GPIO.cleanup()
 
+
 def prizgi_vse():
     for pin in pins:
         GPIO.output(pin, GPIO.LOW)
         print('Prizgan pin', pin)
 
+
 def ugasni_vse():
     for pin in pins:
         GPIO.output(pin, GPIO.HIGH)
         print('Ugasnjen pin', pin)
+
+
+def prizgi_LED(n):
+    '''Prizge n-to led v seznamu.'''
+    if n < len(pins):
+        GPIO.output(pins[n], GPIO.LOW)
+    else:
+        print(n, "presega stevilo priklopljenih luck.")
+
+
+def ugasni_LED(n):
+    '''Ugasne n-to led v seznamu.'''
+    if n < len(pins):
+        GPIO.output(pins[n], GPIO.HIGH)
+    else:
+        print(n, "presega stevilo priklopljenih luck.")
+
+
+def tekoce_LED_naprej():
+    for n in range(len(pins)):
+        prizgi_LED(n)
+
+        time.sleep(0.2)
+        ugasni_LED(n)
+        ugasni_LED(n+1)
+
 try:
     nastavi()
     while True:
         # Dopolni program tako, da bodo utripale vse lučke.
-        prizgi_vse()
-        time.sleep(0.5)
-        ugasni_vse()
-        time.sleep(0.5)
+        tekoce_LED_naprej()
+        time.sleep(0.2)
 
 except KeyboardInterrupt:
     pocisti()
